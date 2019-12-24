970d0a8ecaac7e5a8d7e57a8e87b3dcb3ce72584
Yudong Yu <leon.yuyudong@hotmail.com>
2019-12-24 10:21:57 -0500

    first commit of 3-Memory Management

---
+++ CppND-Memory-Management-Chatbot/src/graphnode.cpp
@@ -0,0 +1,66 @@
+#include "graphedge.h"
+#include "graphnode.h"
+
+#include <iostream>
+
+GraphNode::GraphNode(int id)
+{
+    _id = id;
+}
+
+GraphNode::~GraphNode()
+{
+    //// STUDENT CODE
+    ////
+
+    // if (_chatBot != nullptr) {
+    //     // std::cout << "delete _chatBot" << std::endl;
+    //     delete _chatBot;
+    // } 
+
+    ////
+    //// EOF STUDENT CODE
+}
+
+void GraphNode::AddToken(std::string token)
+{
+    _answers.push_back(token);
+}
+
+void GraphNode::AddEdgeToParentNode(GraphEdge *edge)
+{
+    _parentEdges.push_back(edge);
+}
+
+void GraphNode::AddEdgeToChildNode(GraphEdge *edge)
+{
+    _childEdges.emplace_back(edge);
+}
+
+//// STUDENT CODE
+////
+void GraphNode::MoveChatbotHere(ChatBot chatbot)
+{
+    _chatBot = std::move(chatbot);
+    // std::cout << "inside MoveChatbotHere" << std::endl;
+    _chatBot.SetCurrentNode(this);
+}
+
+void GraphNode::MoveChatbotToNewNode(GraphNode *newNode)
+{
+    newNode->MoveChatbotHere(std::move(_chatBot));
+    // _chatBot = nullptr; // invalidate pointer at source
+}
+////
+//// EOF STUDENT CODE
+
+GraphEdge *GraphNode::GetChildEdgeAtIndex(int index)
+{
+    //// STUDENT CODE
+    ////
+
+    return _childEdges[index].get();
+
+    ////
+    //// EOF STUDENT CODE
+}
